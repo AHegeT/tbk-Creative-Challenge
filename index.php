e3133cd8cd19bd03c69cc23ef8335f4d77c19818
AHegeT <aheget11@gmail.com>
2016-11-29 11:08:28 -0600

    #fixed jittery scroll on Safari #added fonts

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset=UTF-8 />
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon" />
 	<title>Company</title>
     <!-- Latest Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
